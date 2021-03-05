e20bd0adfc3ba094b2212c73ffe989783ca7791b
Shiyan <shiyanboxer7@gmail.com>
2021-03-05 15:02:55 -0500

    frontend and nav

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
+// import axios from "axios";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <NavBar/> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
