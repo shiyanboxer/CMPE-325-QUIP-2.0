081a28188e61531fc405d8cb2f9ebd48f64caef5
Shiyan <shiyanboxer7@gmail.com>
2021-03-06 19:23:12 -0500

    table and search frontend

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 import React from "react"
 // import axios from "axios";
-import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <NavBar/> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Home />
+        </div>       
       </header>
     </div>
   );
